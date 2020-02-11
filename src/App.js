1fe9e468750b3ea37ca5171017eb2d6aeaa70ace
karim mokhtar <karimmokhtar28@gmail.com>
2020-02-11 19:55:38 +0200

    adding the venue Nfo component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from "./components/featured";
+import VenueNfo from './components/venueNfo';
 function App() {
   return (
     <div className="App" style={{height:'1500px',background:'blue'}}>
       <Header />
       <Featured />
+      <VenueNfo />
     </div>
   );
 }
